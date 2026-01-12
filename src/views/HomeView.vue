a7021d2b2ea467c06f00e42fd194e8c425ff5c08
MarcTarrade <marctarrade@gmail.com>
2026-01-12 14:52:11 +0100

    refactor home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,3 @@
-<script>
-import AboutShort from '@/components/About-short.vue';
-export default {
-    components: {
-        AboutShort
-    }
-}
-</script>
-
 <template>
     <div class="wrapper">
     <img src="/lac_allos.jpg" alt="lac_allos" class="background-image">
@@ -14,8 +5,9 @@ export default {
         <div class="abstract-container">
             <p class="abstract-text">Passionné par les technologies web et la cybersécurité, je poursuis actuellement un Master en ingénierie logicielle à l’ISCOD en alternance chez SAP, où je participe à des projets liés à la cyber sécurité. Fort d’expériences variées en développement fullstack, en IA et en sécurité applicative, j’aime concevoir des solutions innovantes, performantes et sécurisées. Curieux, rigoureux et orienté résultats, je cherche à allier excellence technique et impact concret dans mes projets.</p>
         </div>
-        <div class="about-container">
-            <AboutShort></AboutShort>
+        <div class="call-to-action">
+            <button class="project">Voir mes projets</button>
+            <button class="skills">Voir mes compétences</button>
         </div>
     </div>
 </template>
@@ -49,5 +41,28 @@ export default {
         font-size: 1.5rem;
         color: var(--text-on-background);
         text-align: center;
+    }
+    .call-to-action {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        height: 30vh;
+        width: 100%;
+        padding: 0 30%;
+    }
+    .project, .skills {
+        height: 5rem;
+        background-color: var(--secondary-color);
+        color: #FFF;
+        border: none;
+        padding: 0.8rem 3rem;
+        margin: 1rem;
+        font-size: 1.2rem;
+        cursor: pointer;
+        border-radius: 5px;
+        transition: background-color 0.3s ease;
+    }
+    .project:hover, .skills:hover {
+        background-color: var(--primary-color);
     }
 </style>
